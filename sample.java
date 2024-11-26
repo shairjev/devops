0b61caf92fef1536544f965dfdfbeeb473d9a210
jeeva <ja88@outlook.com>
2024-11-26 14:28:23 +0530

    java code added

---
+++ sample.java
@@ -0,0 +1,13 @@
+ Volume in drive C is Windows 
+ Volume Serial Number is 5A20-0479
+
+ Directory of C:\Users\Admin\Desktop\devops
+
+26-11-2024  14:28    <DIR>          .
+26-11-2024  14:20    <DIR>          ..
+26-11-2024  14:20               314 .gitignore
+26-11-2024  14:24               434 a.txt
+26-11-2024  14:20                 8 README.md
+26-11-2024  14:28                 0 sample.java
+               4 File(s)            756 bytes
+               2 Dir(s)  252,662,800,384 bytes free
